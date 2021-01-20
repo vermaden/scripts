dd495b8ab2cb7589e49d7c8e5fbb4e9d562394c7
vermaden <vermaden@interia.pl>
2021-01-20 16:45:02 +0100

    Add Aero Snap Extended script.

--- aero-snap.sh
+++ aero-snap.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Copyright (c) 2018 Slawomir Wojciech Wojtczak (vermaden)
+# Copyright (c) 2021 Slawomir Wojciech Wojtczak (vermaden)
 # All rights reserved.
 #
 # THIS SOFTWARE USES FREEBSD LICENSE (ALSO KNOWN AS 2-CLAUSE BSD LICENSE)
@@ -26,7 +26,7 @@
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # ------------------------------
-# ADVANCED OPEN SOURCE AERO SNAP
+# OPEN SOURCE AERO SNAP EXTENDED
 # ------------------------------
 # vermaden [AT] interia [DOT] pl
 # https://vermaden.wordpress.com
@@ -36,10 +36,11 @@ OVERHEAD_Y=5
 MARGIN_TOP=29
 MARGIN_LEFT=1
 MARGIN_RIGHT=1
+XDPYINFO=$( xdpyinfo | awk '/dimension/ {print $2}' )
 
 snap() {
-  SCREEN_W=$( xdpyinfo | awk '/dimension/ {print $2}' | awk -F 'x' '{print $1}' )
-  SCREEN_H=$( xdpyinfo | awk '/dimension/ {print $2}' | awk -F 'x' '{print $2}' )
+  SCREEN_W=$( echo "${XDPYINFO}" | awk -F 'x' '{print $1}' )
+  SCREEN_H=$( echo "${XDPYINFO}" | awk -F 'x' '{print $2}' )
   case ${1} in
     (L)
       wmctrl -r :ACTIVE: -b remove,maximized_horz
@@ -109,13 +110,13 @@ snap() {
       X=${MARGIN_LEFT}
       Y=${MARGIN_TOP}
       wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-      wmctrl -r :ACTIVE: -e 0,${X},${Y},$(( ( ${SCREEN_W} / 2 ) - ${MARGIN_LEFT} - 4 )),$(( ( ${SCREEN_H} / 2 ) - ${MARGIN_TOP} - 3 )),-1
+      wmctrl -r :ACTIVE: -e 0,${X},${Y},$(( ( ${SCREEN_W} / 2 ) - ${MARGIN_LEFT} - 4 )),$(( ( ${SCREEN_H} / 2 ) - ${MARGIN_TOP} - 4 )),-1
       ;;
     (TR)
       X=$(( ( ${SCREEN_W} / 2 ) - 1 ))
       Y=${MARGIN_TOP}
       wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-      wmctrl -r :ACTIVE: -e 0,${X},${Y},$(( ( ${SCREEN_W} / 2 ) - 3 )),$(( ( ${SCREEN_H} / 2 ) - ${MARGIN_TOP} )),-1
+      wmctrl -r :ACTIVE: -e 0,${X},${Y},$(( ( ${SCREEN_W} / 2 ) - 4 )),$(( ( ${SCREEN_H} / 2 ) - ${MARGIN_TOP} )),-1
       ;;
     (BL)
       X=${MARGIN_LEFT}
@@ -153,23 +154,44 @@ snap() {
       wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
       wmctrl -r :ACTIVE: -e 0,${X},${Y},$(( ${SCREEN_W} / 3 - 5 )),$(( ( ${SCREEN_H} / 3 ) - ${MARGIN_TOP} - ( ${MARGIN_TOP} / 2 ) - 6 )),-1
       ;;
-    (F)
-      wmctrl -r :ACTIVE: -b toggle,maximized_vert,maximized_horz
-      exit 0
-      ;;
-    (Q)
-      wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
-      exit 0
-      ;;
     (*)
-      echo "usage: ..."
+      NAME=${0##*/}
+      echo "usage:"
+      echo
+      echo "  ${0##*/} OPTION"
+      echo
+      echo "OPTION(s):"
+      echo
+      echo "  L - place window on left  half of screen"
+      echo "  R - place window on right half of screen"
+      echo "  T - place window on upper half of screen"
+      echo "  B - place window on lower half of screen"
+      echo
+      echo "  SHIFT-L - place window on left  half of screen taking 2/3 space"
+      echo "  SHIFT-R - place window on right half of screen taking 1/3 space"
+      echo "  SHIFT-T - place window on upper half of screen taking 2/3 space"
+      echo "  SHIFT-B - place window on lower half of screen taking 1/3 space"
+      echo
+      echo "  TL - place window to left/upper  part of screen"
+      echo "  TR - place window to left/lower  part of screen"
+      echo "  BL - place window to right/upper part of screen"
+      echo "  BR - place window to right/lower part of screen"
+      echo
+      echo "  SHIFT-TL - use left/upper  part with 2/3 of H. and 2/3 V. space"
+      echo "  SHIFT-TR - use left/lower  part with 2/3 of H. and 1/3 V. space"
+      echo "  SHIFT-BL - use right/upper part with 1/3 of H. and 2/3 V. space"
+      echo "  SHIFT-BR - use right/lower part with 1/3 of H. and 1/3 V. space"
+      echo
+      echo "  C - center window covering about 2/3 of screen"
+      echo "  F - make current window go fullscreen"
+      echo "  Q - remove fullscreen property from window"
+      echo
       exit 1
       ;;
   esac
 
 }
 
-# wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz
 snap ${1}
 
-echo '1' >> ~/scripts/stats/${0##*/}
+echo '1' 2> /dev/null >> ~/scripts/stats/${0##*/}
