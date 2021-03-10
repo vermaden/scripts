662d635d6e1018e8f4f4b7db73c5690aad2b05dd
vermaden <vermaden@interia.pl>
2021-03-10 10:06:23 +0100

    Update ThinkPad fan script.

--- acpi-thinkpad-fan.sh
+++ acpi-thinkpad-fan.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+# PUT SOMETHING LIKE THAT INTO crontab -e COMMAND
+#   ACPI/IBM/FAN
+#   * * * * * ~/scripts/acpi-thinkpad-fan.sh
+
 if ! kldstat | grep -q acpi_ibm.ko
 then
   doas kldload acpi_ibm 1> /dev/null 2> /dev/null
@@ -8,9 +12,16 @@ fi
 doas sysctl dev.acpi_ibm.0.fan=0 1> /dev/null 2> /dev/null
 
 TEMP=$( sysctl -n hw.acpi.thermal.tz0.temperature | awk -F'.' '{print $1}' )
-
-[ ${TEMP} -lt 50 ] && doas sysctl dev.acpi_ibm.0.fan_level=0 1> /dev/null 2> /dev/null && exit 0
-[ ${TEMP} -lt 60 ] && doas sysctl dev.acpi_ibm.0.fan_level=1 1> /dev/null 2> /dev/null && exit 0
-[ ${TEMP} -lt 70 ] && doas sysctl dev.acpi_ibm.0.fan_level=3 1> /dev/null 2> /dev/null && exit 0
-[ ${TEMP} -lt 80 ] && doas sysctl dev.acpi_ibm.0.fan_level=5 1> /dev/null 2> /dev/null && exit 0
+[ ${TEMP} -lt 40 ] && doas sysctl dev.acpi_ibm.0.fan_level=0 1> /dev/null 2> /dev/null && exit 0
+[ ${TEMP} -lt 50 ] && doas sysctl dev.acpi_ibm.0.fan_level=1 1> /dev/null 2> /dev/null && exit 0
+[ ${TEMP} -lt 60 ] && doas sysctl dev.acpi_ibm.0.fan_level=2 1> /dev/null 2> /dev/null && exit 0
+[ ${TEMP} -lt 70 ] && doas sysctl dev.acpi_ibm.0.fan_level=4 1> /dev/null 2> /dev/null && exit 0
+[ ${TEMP} -lt 80 ] && doas sysctl dev.acpi_ibm.0.fan_level=6 1> /dev/null 2> /dev/null && exit 0
 [ ${TEMP} -gt 80 ] && doas sysctl dev.acpi_ibm.0.fan_level=7 1> /dev/null 2> /dev/null && exit 0
+
+# [ ${TEMP} -lt 50 ] && doas sysctl dev.acpi_ibm.0.fan_level=0 1> /dev/null 2> /dev/null && exit 0
+# [ ${TEMP} -lt 60 ] && doas sysctl dev.acpi_ibm.0.fan_level=1 1> /dev/null 2> /dev/null && exit 0
+# [ ${TEMP} -lt 70 ] && doas sysctl dev.acpi_ibm.0.fan_level=3 1> /dev/null 2> /dev/null && exit 0
+# [ ${TEMP} -lt 80 ] && doas sysctl dev.acpi_ibm.0.fan_level=5 1> /dev/null 2> /dev/null && exit 0
+# [ ${TEMP} -gt 80 ] && doas sysctl dev.acpi_ibm.0.fan_level=7 1> /dev/null 2> /dev/null && exit 0
+
