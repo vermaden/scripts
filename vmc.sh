4595cab60df059f4e7de06fa8e2f5eab24e23a7b
vermaden <vermaden@interia.pl>
2025-02-21 21:30:33 +0100

    Add -s option to show VMs size. Read VM_DIR from /etc/rc.conf file. Upgrade removing lock files. Add option to reset/destroy VM state. Add check for doas(1) and sudo(8) existence.

--- vmc.sh
+++ vmc.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-# Copyright (c) 2023-2024 Slawomir Wojciech Wojtczak (vermaden)
+# Copyright (c) 2023-2025 Slawomir Wojciech Wojtczak (vermaden)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -23,7 +23,7 @@
 # THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #
 # ------------------------------
-# BHYVE(8) VMs PAUSE/RESUME TOOL
+# BHYVE(8) VMs LIST/CMD/FIX TOOL
 # ------------------------------
 # vermaden [AT] interia [DOT] pl
 # https://vermaden.wordpress.com
@@ -40,8 +40,8 @@
   COLW='\\e[1;37m' # white
   REGEX_MAC='[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}:[0-9a-f]{2}'
   REGEX_UUID='[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}'
-  VM_DIR=/vm
 
+# HELP DISPLAY FUNCTION
 __usage() {
   NAME=${0##*/}
   cat << HELP
@@ -51,33 +51,93 @@ usage:
   ${NAME} -l | --unlock  -  unlock all locked VMs
   ${NAME} -m | --mac     -  check for duplicated MAC addresses in VMs
   ${NAME} -u | --uuid    -  check for duplicated UUID values in VMs
+  ${NAME} -d | --destroy -  destroy VM and its resources
+  ${NAME} -s | --size    -  show size of VMs
 
 HELP
   exit 1
 }
 
+# MATH FUNCTION
+__math() {
+  local SCALE=1
+  local INPUT=$( echo "${@}" | tr 'x' '*' | tr ',' '.' )
+  local RESULT=$( echo "scale=${SCALE}; ${INPUT}" | bc -l )
+  if echo ${RESULT} | grep -q '^\.'
+  then
+    echo -n 0
+  fi
+  echo ${RESULT}
+}
+
+# CHECK IF vm-bhyve IS INSTALLED
 unalias vm    1> /dev/null 2> /dev/null
 if ! which vm 1> /dev/null 2> /dev/null
 then
   echo "NOPE: install 'sysutils/vm-bhyve-devel' package"
 fi
 
+# CHECK doas(1) AND/OR sudo(8) EXISTENCE
+unalias doas
+unalias sudo
+if which doas 1> /dev/null 2> /dev/null
+then
+  DOAS=doas
+elif which sudo 1> /dev/null 2> /dev/null
+then
+  DOAS=sudo
+else
+  DOAS=''
+fi
+
+# GET ${VM_DIR} VALUE FROM /etc/rc.conf FILE
+  VM_DATASET=$( \
+    grep '^[\ \t]*[^\t\ #]' /etc/rc.conf \
+      | grep vm_dir \
+      | awk -F '=' '{print $2}' \
+      | tr -d \" \
+      | sed s.zfs:..g )
+  VM_DIR=$(  zfs get -o value -H mountpoint ${VM_DATASET} )
+  VM_DIR_BACK=$( echo "${VM_DIR}" | sed 's./.\\/.g' )
+
+# CHECK PASSED ARGUMENTS
 case ${1} in
+
+  # PRINT HELP INFORMATION
   (-h|--help|-help|help)
     __usage
     ;;
 
+  # DESTROY MACHINE AND ITS RESOURCES
+  (-d|--destroy)
+    if [ ${2} ]
+    then
+      ${DOAS} bhyvectl --destroy --vm=${2}
+      if [ ${?} -eq 0 ]
+      then
+        echo "INFO: VMs '${2}' destroyed successfully"
+        exit 1
+      fi
+    fi
+    ;;
+
+  # UNLOCK ALL LOCKED VMS
   (-l|--unlock)
     if [ "${VM_DIR}" != "" ]
     then
       cd ${VM_DIR}
-      echo "INFO: VMs with 'run.lock' file:"
-      ls */run.lock
-      doas find ${VM_DIR} -name run.lock -delete
+      VM_DIR_LOCK_LS=$( ls */run.lock 2> /dev/null )
+      if [ "${VM_DIR_LOCK_LS}" != "" ]
+      then
+        echo "INFO: VMs with 'run.lock' file:"
+        echo "${VM_DIR_LOCK_LS}"
+      fi
+      ${DOAS} find ${VM_DIR} -name run.lock -delete
     fi
     exit 0
     ;;
 
+  # CHECK FOR DUPLICATED MAC ADDRESSES IN MACHINES
   (-m|--mac)
     cd ${VM_DIR}
     CONFS_MACS_LIST=$( grep -i mac */*.conf )
@@ -88,7 +148,7 @@ case ${1} in
       | while read MAC
         do
           echo "INFO: VMs with '${MAC}' mac address:"
-          cd ${VM_DIR}
+          cd "${VM_DIR}"
           grep -r -i ${MAC} */*.conf \
             | awk -F':' '{print $1}' \
             | sed -e 's|.conf||g' \
@@ -101,8 +161,9 @@ case ${1} in
     exit 0
     ;;
 
+  # CHECK FOR DUPLICATED UUID VALUES IN MACHINES
   (-u|--uuid)
-    cd ${VM_DIR}
+    cd "${VM_DIR}"
     CONFS_MACS_LIST=$( grep -i uuid */*.conf )
     echo "${CONFS_MACS_LIST}" \
       | grep -i -o -E "${REGEX_UUID}" \
@@ -111,7 +172,7 @@ case ${1} in
       | while read UUID
         do
           echo "INFO: VMs with '${UUID}' mac address:"
-          cd ${VM_DIR}
+          cd "${VM_DIR}"
           grep -r -i ${UUID} */*.conf \
             | awk -F':' '{print $1}' \
             | sed -e 's|.conf||g' \
@@ -124,10 +185,33 @@ case ${1} in
     exit 0
     ;;
 
+  # PRINT MACHINES SIZES
+  (-s|--size)
+    printf "%9s  %s\n" 'SPACE' 'VM'
+    TOTAL=0
+    while read NAME DATASTORE LOADER CPU MEMORY VNC AUTO STATE GARBAGE
+    do
+      SIZE=$( ${DOAS} vm info ${NAME} \
+                | awk '/bytes-used:/ {print $2}' \
+                | while read DISK
+                  do
+                    SUM=$(( ${SUM} + ${DISK} ))
+                    echo ${SUM}
+                  done | tail -1 )
+      TOTAL=$(( ${SIZE} + ${TOTAL} ))
+      SIZE=$( __math ${SIZE} / 1024 / 1024 / 1024 )
+      printf "%7.1fGB  %s\n" ${SIZE} ${NAME}
+    done << EOF
+  $( ${DOAS} vm list | sed 1d )
+EOF
+    TOTAL=$( __math ${TOTAL} / 1024 / 1024 / 1024 )
+    printf "%7.1fGB  TOTAL\n" ${TOTAL}
+    exit 0
+    ;;
+
 esac
 
-VM_LIST=$( doas vm list 2> /dev/null || sudo vm list 2> /dev/null )
-
+# NO OPTIONS GIVEN - JUST PRINT LIST OF VMS
 while read LINE
 do
   # GREEN COLOR FOR 'Running' MACHINES
@@ -162,13 +246,9 @@ do
     continue
   fi
 
+  # REPEAT POWERED OFF MACHINES
   echo -e "${LINE}"
 
 done << EOF
-  $( echo "${VM_LIST}" )
+  $( ${DOAS} vm list 2> /dev/null || sudo vm list 2> /dev/null )
 EOF
-
-
-
-
-
